8f16a0d18124f4e95b0d5918612d78bc7aaa75a4
Shivya Shukla <shivyashukla15@gmail.com>
2024-09-03 15:36:46 +0530

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
