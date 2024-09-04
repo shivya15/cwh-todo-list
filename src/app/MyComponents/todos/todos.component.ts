f197a02f7a38c415c06b195919fdddc7f6999437
Shivya Shukla <shivyashukla15@gmail.com>
2024-09-04 22:27:22 +0530

    Added LocalStorage

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -6,38 +6,32 @@ import { Todo } from "../../Todo";
   styleUrl: './todos.component.css'
 })
 export class TodosComponent {
+ localItem: string;
  todos: Todo[];
  constructor(){
-  this.todos=[
-    {
-      sno:1,
-      title: "This is title",
-      desc: "Description",
-      active:true
-    },
-    {
-      sno:2,
-      title: "This is title 2",
-      desc: "Description",
-      active:true
-    },
-    {
-      sno:3,
-      title: "This is title 3",
-      desc: "Description",
-      active:true
+  if (typeof localStorage !== 'undefined') {
+    this.localItem = localStorage.getItem("todos") || '';
+    if (this.localItem === '') {
+      this.todos = [];
+    } else {
+      this.todos = JSON.parse(this.localItem);
     }
-    
-  ]
+  } else {
+    this.localItem = '';
+    this.todos = []; // Provide a fallback in case localStorage is not available
+  }
+  
  }
 
  deleteTodo(todo:Todo){
   console.log(todo);
   const index= this.todos.indexOf(todo);
   this.todos.splice(index,1);
+  localStorage.setItem("todos",JSON.stringify(this.todos));
  }
  addTodo(todo:Todo){
   console.log(todo);
   this.todos.push(todo);
+  localStorage.setItem("todos",JSON.stringify(this.todos));
  }
 }
